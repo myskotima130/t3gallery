942a68e323ca397a9f6b02597d345616d06c610c
Tymofii Mysko <mysko@tymofiis-mbp.myfiosgateway.com>
2024-08-22 15:45:08 -0400

    add images

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 ## TODO
 
-- [ ] Make it deploy (vercel)
+- [x] Make it deploy (vercel)
 - [ ] Scaffold basic ui with mock data
+- [ ] Actually set up a database (vercel postgres)
+- [ ] Attach database to UI
+- [ ] Add authentication (w/ clerk)
+- [ ] Add image upload
+- [ ] Error management (w/ Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete button (w/ Server Actions)
+- [ ] Analytics (posthog)
+- [ ] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/734438af-53e6-45b6-892f-83b74a33d9e4-22to.jpeg",
+  "https://utfs.io/f/b18ae6b3-5a4f-4cfc-af00-84af1f65c383-1sj3pb.png",
+  "https://utfs.io/f/0c66654a-58b2-4110-b1ab-c50eaddbacab-2tb.png",
+  "https://utfs.io/f/8c241629-2bac-4f67-aad0-c36968e78570-20eer.webp",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div className="w-48" key={image.id}>
+            <img src={image.url} alt={image.id} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
