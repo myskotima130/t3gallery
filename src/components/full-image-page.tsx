8a0f41b1440b39c450b0415d61d4e0ef645f9b09
myskotima130 <myskotima130@gmail.com>
2024-09-07 19:35:23 -0400

    finalize modal and parallel image routes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,21 +1,32 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import React from "react";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex min-w-[80%] flex-shrink items-center justify-center">
+      <div className="flex w-full flex-shrink items-center justify-center">
         <img
           src={image.url}
           alt={image.name}
-          className="max-h-96 flex-shrink-0 object-contain"
+          className="max-h-[80%] flex-shrink object-contain"
         />
       </div>
 
-      <div className="flex w-48 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+
+        <div className="flex flex-col p-2">
+          <span>Uploaded By</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created On</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
